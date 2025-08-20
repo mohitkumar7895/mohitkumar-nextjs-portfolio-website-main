58297e7bf93c4eb991ae9a8d4eaed6a8aeb526cc
mohitkumar7895 <mohitporwal596@gmail.com>
2025-08-20 20:42:11 +0530

    first commit

---
+++ src/contents/blogs.ts
@@ -0,0 +1,46 @@
+import { Blog } from "@/types";
+
+export const blogs: Blog[] = [
+    {
+        title: 'Getting Started with Next.js 15',
+        excerpt: 'Learn about the new features and improvements in Next.js 15.',
+        date: '2025-08-15',
+        readTime: '5 min read',
+        slug: 'getting-started-with-nextjs-15',
+      },
+      {
+        title: 'InterMediate JavaScript for React',
+        excerpt: 'A practical guide to using JavaScript effectively in React applications, designed for intermediate learners to strengthen their skills.',
+        date: '2025-05-10',
+        readTime: '8 min read',
+        slug: 'intermediate-javascript-for-react',
+      },
+      {
+        title: 'Building Responsive Layouts with Tailwind CSS',
+        excerpt: 'Tips and tricks for creating responsive designs using Tailwind CSS.',
+        date: '2025-03-05',
+        readTime: '6 min read',
+        slug: 'building-responsive-layouts-with-tailwind-css',
+      },
+      {
+        title: 'Understanding React Hooks',
+        excerpt: 'A deep dive into React Hooks and how to use them effectively.',
+        date: '2024-11-01',
+        readTime: '7 min read',
+        slug: 'understanding-react-hooks',
+      },
+        {
+            title: 'Deploying React.js Applications on Vercel',
+            excerpt: 'Step-by-step guide to deploying your React.js applications on Vercel.',
+            date: '2025-4-25',
+            readTime: '4 min read',
+            slug: 'deploying-reactjs-applications-on-vercel',
+        },
+        {
+            title: 'Building a RESTful API with Node.js and Express',
+            excerpt: 'Learn how to create a RESTful API using Node.js and Express.',
+            date: '2025-05-20',
+            readTime: '10 min read',
+            slug: 'building-a-restful-api-with-nodejs-and-express',
+        },
+  ];
---
+++ src/contents/projects.ts
@@ -0,0 +1,69 @@
+import { Project } from "@/types";
+
+
+export const projects: Project[] = [
+    {
+        title: 'Barber Shop Website',
+        description: 'A full-featured barber shop website with online booking, service catalog, customer reviews, and appointment reminders — designed to deliver a smooth and professional user experience',
+        technologies: ['React.js', 'JavaScript', 'Tailwind CSS', 'HTML5', 'CSS3'],
+        githubLink: 'https://github.com/mohitkumar7895/Barber-Website',
+        demoLink: 'https://barber-website-o95k.vercel.app/',
+        image: '/projects/barber-shop-website.jpg',
+      },
+      {
+        title: 'Task Management App',
+        description: 'A collaborative task management application with real-time updates.',
+        technologies: ['React', 'Node.js', 'MongoDB', 'Socket.io'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/blog-website.jpg',
+      },
+      {
+        title: 'Portfolio Website',
+        description: 'My personal portfolio website showcasing my projects and skills.',
+        technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/dental-clinic-website.jpg',
+      },
+      {
+        title: 'Blog Platform',
+        description: 'A blogging platform with user authentication and markdown support.',
+        technologies: ['Gatsby', 'GraphQL', 'Contentful'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/e-commerce-website.jpg',
+      },
+      {
+        title: 'Weather App',
+        description: 'A weather application that provides real-time weather updates.',
+        technologies: ['React', 'OpenWeatherMap API'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/hotel-booking-management.jpg',
+      },
+      {
+        title: 'Chat Application',
+        description: 'A real-time chat application using WebSocket technology.',
+        technologies: ['React', 'Node.js', 'Socket.io'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/learn-management-system.jpg',
+      },
+      {
+        title: 'Recipe Finder',
+        description: 'A recipe search application using the Edamam API.',
+        technologies: ['React', 'Edamam API'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/weather-app.jpg',
+      },
+      {
+        title: 'Expense Tracker',
+        description: 'A personal finance tracker to manage expenses and income.',
+        technologies: ['React', 'Firebase'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://demo.com',
+        image: '/projects/expense-tracker.webp',
+      }
+  ];
